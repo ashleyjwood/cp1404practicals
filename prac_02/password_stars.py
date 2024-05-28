2cc4b1a30aa42859080f63565a2d5d912eefbffd
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-05-28 19:29:31 +1000

    Added password checking functionality

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,30 @@
-MINIMUM_LENGTH = 10
-password = input("Enter your password: ")
-while len(password) < MINIMUM_LENGTH:
-    print(f"Password is too short! Must be at least {MINIMUM_LENGTH} characters.")
+"""
+Enter a password and print a row of stars the same length.
+"""
+
+# CONSTANTS
+PASSWORD_LENGTH_MIN = 10
+
+
+def main():
+    password = get_password()
+    print_stars(password)
+
+
+def get_password():
+    """Get a password from the user and check its length."""
     password = input("Enter your password: ")
-for i in range(len(password)):
-    print("*", end="")
-print()
+    while len(password) < PASSWORD_LENGTH_MIN:
+        print(f"Password is too short! Must be at least {PASSWORD_LENGTH_MIN} characters.")
+        password = input("Enter your password: ")
+    return password
+
+
+def print_stars(password):
+    """Print a row of stars the same length as the password."""
+    for i in range(len(password)):
+        print("*", end="")
+    print()
+
+
+main()
