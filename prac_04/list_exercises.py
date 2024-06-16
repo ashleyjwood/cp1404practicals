5c6e973c4aaddfeaac664760b3ee507b06b8683d
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-06-16 13:37:15 +1000

    Investigate details about a list of 5 values

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,11 @@
+numbers = []
+
+for i in range(5):
+    value = int(input("Enter a number: "))
+    numbers.append(value)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average number is {sum(numbers) / len(numbers)}")
