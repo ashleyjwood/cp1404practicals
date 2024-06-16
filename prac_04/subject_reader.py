522b526179b0f35b3fc31cde92857a082bcf9130
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-06-16 13:23:05 +1000

    Refactor subject detail printing

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,33 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    print_subject_details(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    concatenated_list = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        concatenated_list.append(parts)
     input_file.close()
+    return concatenated_list
+
+
+def print_subject_details(data):
+    """Print subject details with string length handling."""
+    max_lecturer_width = max(len(parts[1]) for parts in data)
+    max_students_width = max(len(str(parts[2])) for parts in data)
+    for parts in data:
+        subject = parts[0]
+        lecturer = parts[1]
+        number_of_students = parts[2]
+        print(
+            f"{subject} is taught by {lecturer:{max_lecturer_width}} and has {number_of_students:{max_students_width}} "
+            f"students")
 
 
 main()
