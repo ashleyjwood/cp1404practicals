1ce52150753fb87f81a763821898ff1ddbe0f1f6
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-07-08 14:42:28 +1000

    Complete languages.py and programming_language.py practical

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,4 +1,15 @@
 """
 Estimated time: 30 minutes
-Actual time:
+Actual time: 45 minutes
 """
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+
+programming_languages = [python, ruby, visual_basic]
+dynamic_languages = [language.name for language in programming_languages if language.is_dynamic()]
+print(f"The dynamically typed languages are: \n{'\n'.join(dynamic_languages)}")
