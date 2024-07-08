4990b15dcdda23a898c00e9199187364cedd0077
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-07-08 13:44:04 +1000

    Initial commit

---
+++ prac_06/programming_language.py
@@ -0,0 +1,10 @@
+class ProgrammingLanguage:
+    """Represents a programming language object."""
+
+    def __init__(self, name=""):
+        """Initialise a ProgrammingLanguage instance."""
+        pass
+
+    def __str__(self):
+        """Return a string representation of the instance."""
+        pass
