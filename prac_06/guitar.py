e3e58f87d4760e17ab7a4d85ba0ee5cb0bac4f34
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-07-08 16:04:24 +1000

    Complete guitar.py and guitar_test.py for practical

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,10 +1,19 @@
-class ClassName:
-    """Represents a (class name) object."""
+class Guitar:
+    """Represents a guitar object."""
 
-    def __init__(self):
-        """Initialise a (class name) instance."""
-        pass
+    def __init__(self, name="", year=0, cost=0.0):
+        """Initialise a guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def get_age(self, current_year):
+        guitar_age = current_year - self.year
+        return guitar_age
+
+    def is_vintage(self, current_year):
+        return self.get_age(current_year) >= 50
 
     def __str__(self):
         """Return a string representation of the instance."""
-        pass
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,4 +1,24 @@
 """
-Estimated time: 30 minutes
-Actual time:
+Estimated time for guitar.py and guitar_test.py: 30 minutes
+Actual time: ~60 minutes
 """
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Main docstring"""
+    current_year = int(input("Enter current year: "))
+
+    guitar = Guitar(name="Gibson L-5 CES", year=1951, cost=16035.40)  # Corrected release year
+    print(f"{guitar.name} get_age() - Expected 71 (Assuming year is 2022). Got {guitar.get_age(current_year)}.")
+    print(f"{guitar.name} is_vintage() - Expected True. Got {guitar.is_vintage(current_year)}.")
+    print("")  # Blank line for readability
+
+    guitar = Guitar(name="Another Guitar", year=2013)
+    print(f"{guitar.name} get_age() - Expected 9 (Assuming year is 2022). Got {guitar.get_age(current_year)}.")
+    print(f"{guitar.name} is_vintage() - Expected False. Got {guitar.is_vintage(current_year)}.")
+
+
+if __name__ == '__main__':
+    main()
