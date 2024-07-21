23204050990bbe8c903b654cffc87dc8cd7cfe44
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-07-21 19:05:40 +1000

    Refactor to bring in line with practical objectives

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,40 +1,37 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
 
 DISTANCE_CONVERSION = 1.60934  # Constant used to convert miles to kms
 
 
 class ConvertMilesToKms(App):
-    """Kivy App for converting miles to kilometers"""
+    """Kivy App for converting miles to kilometers."""
 
     def build(self):
-        """Build the Kivy app from the kv file"""
-        Window.size = (500, 300)
+        """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def convert_distance(self):
-        try:
-            miles = float(self.root.ids.input_number.text)  # Should this be "mile" (as it's not a list)?
-            kms = miles * DISTANCE_CONVERSION  # Likewise as above, "km"?
-            self.root.ids.output_label.text = f"{kms:.5f}"
-        except ValueError:
-            self.root.ids.output_label.text = "Invalid input"
+    def calculate_conversion(self):
+        """Calculate distance in kilometres."""
+        miles = self.get_valid_input()
+        kms = miles * DISTANCE_CONVERSION
+        self.root.ids.output_label.text = f"{kms:.5f}"  # Handle odd cases such as 0.1mi->0.16093400000000002km
 
-    def handle_increment(self, button_id):
-        """Handle +ve or -ve increment button press, with error checking for invalid inputs."""
+    def handle_increment(self, speed_delta):
+        """Handle +ve or -ve increment button press."""
+        miles = self.get_valid_input() + speed_delta
+        self.root.ids.input_number.text = str(miles)
+        self.calculate_conversion()
+
+    def get_valid_input(self):
+        """Get a valid number input from the user."""
         try:
             miles = float(self.root.ids.input_number.text)
+            return miles
         except ValueError:
-            miles = 0  # If the text entered is not a valid number, initialise to zero and then handle button press
-
-        if button_id == "mile_up":
-            miles += 1.0
-        elif button_id == "mile_down":
-            miles -= 1.0
-        self.root.ids.input_number.text = str(miles)
+            return 0
 
 
 ConvertMilesToKms().run()
