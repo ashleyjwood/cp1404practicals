74900f9cf1b42c59f86ec93a3806cef8bdcd861d
ashleyjwood <ashley.wood@my.jcu.edu.au>
2024-08-04 11:44:48 +1000

    Refactor price_per_km to be a class variable

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,8 +2,7 @@ from taxi import Taxi
 
 
 def main():
-    my_taxi = Taxi("Prius 1", 100,
-                   1.23)  # Create a new taxi object, my_taxi, with the name "Prius 1", 100 units of fuel, and a price of $1.23
+    my_taxi = Taxi("Prius 1", 100)  # Create a new taxi object, my_taxi, with the name "Prius 1", 100 units of fuel, and a price of $1.23
     my_taxi.drive(40)  # Drive 40km
     print(f"Taxi details: {my_taxi};\nCost: ${my_taxi.get_fare()}")  # Print taxi details and current fair
     my_taxi.start_fare()  # Start a new fare
